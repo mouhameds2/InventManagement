6b78db469bf3026665843b68fdda1cd8c90f56ad
Mouhamed <mouhameds2@hotmail.fr>
2022-06-13 18:03:56 +0200

    first commit

---
+++ src/main/java/fallit/com/Inventaire/InventaireApplication.java
@@ -0,0 +1,13 @@
+package fallit.com.Inventaire;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventaireApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventaireApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fallit/com/Inventaire/InventaireApplicationTests.java
@@ -0,0 +1,13 @@
+package fallit.com.Inventaire;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventaireApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
